40f6f7f124066e953ea7ea99751f6297bbbe305c
blackruby <ruby@blackruby.hu>
2025-12-11 21:50:11 +0100

    Added util functions for sanitizing urls to a common style

---
+++ src/main/java/com/yaoiyun/yyscrape/utils/UrlUtils.java
@@ -0,0 +1,17 @@
+package com.yaoiyun.yyscrape.utils;
+
+public class UrlUtils {
+
+    // TODO split the url with and get the [1] element, that should be the xyz.com base url part.
+    public static String getBaseUrl() {
+        return "";
+    }
+
+    public static String getSanitizedUrl(String url) {
+        if(url.endsWith("/")) {
+            return url.toLowerCase();
+        }
+
+        return url.toLowerCase() + "/";
+    }
+}
