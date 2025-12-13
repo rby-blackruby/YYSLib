bccb44a0d4bae080920675158bc2de080471b706
blackruby <ruby@blackruby.hu>
2025-12-13 20:21:07 +0100

    broken up the scraper interface into subinterfaces

---
+++ src/main/java/com/yaoiyun/yyscrape/model/ScrapableContentFactory.java
@@ -0,0 +1,25 @@
+package com.yaoiyun.yyscrape.model;
+
+import com.yaoiyun.yyscrape.model.implementations.BattwoManhwa;
+import com.yaoiyun.yyscrape.model.implementations.MangabuddyManhwa;
+
+public class ScrapableContentFactory {
+
+    private ScrapableContentFactory() {
+
+    }
+
+    public static <T extends ScrapableContent> T getContent(String name, String url, ScrapableContentType contentType) {
+        ScrapableContent content = switch(contentType) {
+            case ScrapableContentType.IMAGE -> ManhwaFactory.getManhwa(name, url);
+
+            // TODO: switch these out to actually implemented factories
+            case ScrapableContentType.TEXT -> new BattwoManhwa("Name", "Url");
+            case ScrapableContentType.VIDEO -> new MangabuddyManhwa("Name", "Url");
+        };
+
+        @SuppressWarnings("unchecked")
+        T c = (T) content;
+        return c;
+    }
+}
