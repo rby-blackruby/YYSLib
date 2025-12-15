775e0505c808666d03c8f656219c0cf3a9c5f717
blackruby <ruby@blackruby.hu>
2025-12-16 00:24:33 +0100

    got a basic implementation of get content urls working

--- src/main/java/com/yaoiyun/yyscrape/scraper/implementations/ManhwaLinkExtractor.java
+++ src/main/java/com/yaoiyun/yyscrape/scraper/implementations/ManhwaLinkExtractor.java
@@ -2,9 +2,16 @@ package com.yaoiyun.yyscrape.scraper.implementations;
 
 import com.yaoiyun.yyscrape.content.Manhwa;
 import com.yaoiyun.yyscrape.scraper.LinkExtractor;
+import com.yaoiyun.yyscrape.utils.UrlUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import java.time.Duration;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class ManhwaLinkExtractor extends LinkExtractor<Manhwa> {
 
@@ -14,6 +21,21 @@ public class ManhwaLinkExtractor extends LinkExtractor<Manhwa> {
 
     @Override
     public List<String> getContentUrls() {
+        this.getWebDriver().get(this.getAssignedContent().getUrl());
+        this.getWebDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        List<WebElement> elements = this.getWebDriver().findElements(By.cssSelector("a"));
+        List<String> hrefs = elements.stream()
+                .map(e -> e.getAttribute("href"))
+                .filter(Objects::nonNull)
+                .filter(href -> href.contains(UrlUtils.getSanitizedUrl(this.getAssignedContent().getUrl()) + this.getAssignedContent().getManhwaConfig().getChapterUrlRegex()))
+                .distinct()
+                .toList();
+
+        for(var href : hrefs) {
+            System.out.println(href);
+        }
+
         return List.of();
     }
 
