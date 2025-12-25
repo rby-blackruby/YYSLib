1333946092ecdc8034bb0438eeba9b2090a4f483
blackruby <ruby@blackruby.hu>
2025-12-25 01:04:42 +0100

    added base64 image type

--- src/main/java/com/yaoiyun/yyscrape/content/ScrapableContentType.java
+++ src/main/java/com/yaoiyun/yyscrape/content/ScrapableContentType.java
@@ -1,6 +1,7 @@
 package com.yaoiyun.yyscrape.content;
 
 public enum ScrapableContentType {
+    BASE64_IMAGE, // zinchan type manhwa sites display images as base64 encoded strings
     IMAGE,
     VIDEO,
     TEXT
