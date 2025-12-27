92694ce540c0da9396f157d5af466c41c57fceb0
blackruby <ruby@blackruby.hu>
2025-12-27 03:19:45 +0100

    uses ScraperSupplier instead to use function parameter

--- src/main/java/com/yaoiyun/yyscrape/scraper/ContentScraperFactory.java
+++ src/main/java/com/yaoiyun/yyscrape/scraper/ContentScraperFactory.java
@@ -1,24 +1,29 @@
 package com.yaoiyun.yyscrape.scraper;
 
+import com.yaoiyun.yyscrape.content.ScrapableContent;
+import com.yaoiyun.yyscrape.utils.UrlUtils;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 
 public final class ContentScraperFactory {
-    private static final Map<String, Supplier<? extends ContentScraper>> CONTENT_SCRAPER_REGISTRY = new ConcurrentHashMap<>();
+    private static final Map<String, ScraperSupplier<? extends ContentScraper>> CONTENT_SCRAPER_REGISTRY = new ConcurrentHashMap<>();
 
     private ContentScraperFactory() {
     }
 
-    public static void register(String websiteBaseUrl, Supplier<? extends ContentScraper> contentScraperClass) {
+    public static void register(String websiteBaseUrl, ScraperSupplier<? extends ContentScraper> contentScraperClass) {
         CONTENT_SCRAPER_REGISTRY.putIfAbsent(websiteBaseUrl, contentScraperClass);
     }
 
-    public static ContentScraper getContentScraper(String websiteBaseUrl) {
+    public static ContentScraper getContentScraperFor(ScrapableContent assignedContent) {
+        String websiteBaseUrl = UrlUtils.getBaseUrl(assignedContent.getUrl());
+
         if (!CONTENT_SCRAPER_REGISTRY.containsKey(websiteBaseUrl)) {
             throw new IllegalArgumentException("The provided url is not associated with any scrapper: " + websiteBaseUrl);
         }
 
-        return CONTENT_SCRAPER_REGISTRY.get(websiteBaseUrl).get();
+        return CONTENT_SCRAPER_REGISTRY.get(websiteBaseUrl).get(assignedContent);
     }
 }
