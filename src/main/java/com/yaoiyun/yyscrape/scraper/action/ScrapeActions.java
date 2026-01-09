8bb8de99b80b34c64338262b8400c799af3e4438
blackruby <ruby@blackruby.hu>
2026-01-09 14:20:05 +0100

    added default empty implementation of on page loaded action

--- src/main/java/com/yaoiyun/yyscrape/scraper/action/ScrapeActions.java
+++ src/main/java/com/yaoiyun/yyscrape/scraper/action/ScrapeActions.java
@@ -3,5 +3,7 @@ package com.yaoiyun.yyscrape.scraper.action;
 import org.openqa.selenium.WebDriver;
 
 public interface ScrapeActions {
-    void onPageLoadedAction(WebDriver webDriver);
+    default void onPageLoadedAction(WebDriver webDriver) {
+        // intentionally empty for no action done
+    }
 }
